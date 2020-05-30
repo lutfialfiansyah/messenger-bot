0d5c71345bc6ab82a18b75421be8158bb4f346f5
Muhamad Lutfi Alfiansyah <lutfialfiansyah28@gmail.com>
2020-05-30 14:14:17 +0700

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
+app.get('/', function(req, res){
+    res.send("Hi I'm a bot.");
+});
+
+//facebook
+app.get('/webhook/', function(req, res){
+    if (req.query['hub.verify_token'] === "blondiebytes") {
+        res.query(req.query['hub.challenge']);
+    }
+    res.send("Wrong token");
+});
+
 app.listen(3000, () => console.log('Webhook server is listening port 3000'));
